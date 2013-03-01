c0d99ffa9bb22e61ae226cc8210e8234ecd1aee7
jasonblanchard <jas.blanchard@gmail.com>
2013-03-01 16:45:59 -0500

    Starting to build out the story controller

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,4 +2,23 @@ class StoriesController < ApplicationController
     def index
         @stories = Story.all
     end
+
+    def show
+        @story = Story.find(params[:id])
+    end
+
+    def new
+        @story = current_user.stories.new
+    end
+
+    def create
+        @story = current_user.stories.new(params[:story])
+
+        if @story.save
+            redirect_to @story
+        else
+            render_action :create
+        end
+    end
+
 end
