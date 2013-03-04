7e447e5881cc85ea841f6c8c85c7bd7641cf21a4
jasonblanchard <jas.blanchard@gmail.com>
2013-03-04 10:19:22 -0500

    Added sentence validation

--- app/models/slice.rb
+++ app/models/slice.rb
@@ -4,4 +4,18 @@ class Slice < ActiveRecord::Base
   attr_accessible :body
 
   validates :body, :presence => true
+  validate :does_not_excede_max_sentences
+
+  def does_not_excede_max_sentences
+      if !body.blank?
+        sentences = body.split(/((?<=[a-z0-9)][.?!])|(?<=[a-z0-9][.?!]"))\s+(?="?[A-Z])/)
+        sentences.delete("")
+        max_sentences = self.story.max_sentences
+
+        if sentences.length > self.story.max_sentences
+            errors.add(:body, " must have less than #{max_sentences} sentences.")
+        end
+      end
+  end
+
 end
