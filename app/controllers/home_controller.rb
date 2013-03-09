0e551bcd335a86faa0cd8f291100b0fd1f95d230
jasonblanchard <jas.blanchard@gmail.com>
2013-03-09 10:40:36 -0500

    Ordering stories on hoempage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
 
     def home
-        @complete_stories = Story.where("complete = ?", true).limit(5)
-        @incomplete_stories = Story.where("complete = ?", false).limit(5)
+        @complete_stories = Story.where("complete = ?", true).limit(5).order('created_at DESC')
+        @incomplete_stories = Story.where("complete = ?", false).limit(5).order('created_at DESC')
     end 
 
 end
