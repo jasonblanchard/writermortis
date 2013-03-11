614cd07bf69964cdfe4e05c82d822bff89214199
jasonblanchard <jas.blanchard@gmail.com>
2013-03-11 11:01:59 -0400

    Added form validations for creating a story

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,6 +2,8 @@ class Story < ActiveRecord::Base
   attr_accessible :title, :max_sentences, :total_slices, :complete, :user_id
 
   validates :title, :presence => true
+  validates :total_slices, :presence => true, :numericality => { :only_integer => true }
+  validates :max_sentences, :presence => true, :numericality => { :only_integer => true }
 
   belongs_to :user
   has_many :slices
