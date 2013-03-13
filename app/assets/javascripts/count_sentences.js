7c7f86b8b51b6d922355876bc3ddfd539401e2ec
jasonblanchard <jas.blanchard@gmail.com>
2013-03-13 13:35:38 -0400

    Fixed js to update on copy-paste

--- app/assets/javascripts/count_sentences.js
+++ app/assets/javascripts/count_sentences.js
@@ -1,8 +1,21 @@
 $(document).ready(function() {
 
-    if ($('input[name="commit"]')) {
+    if ($('textarea#slice_body').length > 0) {
 
         $('input[name="commit"]').after('<div class="sentence-update pull-right">You have written 0 sentences</div>');
+        
+        $('textarea#slice_body').bind('input propertychange', function() {
+            $('.sentence-update').html('');
+
+            var sentences = $('textarea#slice_body').val().match(/([^\r\n.!?]+([.!?]+|$))/gim);
+
+            if (sentences != null) {
+                $('.sentence-update').html('You have written ' + sentences.length + ' sentences.');
+            } else {
+                $('.sentence-update').html('You have written 0 sentences');
+            }
+
+        });
 
         $('#slice_body').keyup(function() {
 
