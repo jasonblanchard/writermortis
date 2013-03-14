f6615c4be6d37581aaba1d293cb271b195ee473b
jasonblanchard <jas.blanchard@gmail.com>
2013-03-14 12:28:03 -0400

    Different home page for logged in users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,14 @@
 class HomeController < ApplicationController
 
     def home
-        @complete_stories = Story.where("complete = ?", true).limit(5).order('created_at DESC')
-        @incomplete_stories = Story.where("complete = ?", false).limit(5).order('created_at DESC')
-    end 
+        @complete_stories = Story.where("complete = ?", true).limit(10).order('created_at DESC')
+        @incomplete_stories = Story.where("complete = ?", false).limit(10).order('created_at DESC')
+
+        if user_signed_in?
+            render :template => 'home/logged_in_home'
+        else
+            render :template => 'home/home'
+        end
+    end
 
 end
