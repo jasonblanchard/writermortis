763277c0b371b85a133e3c8e2b7bf3cf54c9346f
jasonblanchard <jas.blanchard@gmail.com>
2013-03-14 16:55:17 -0400

    Refactored tests to use factory girl

--- spec/models/slice_model_spec.rb
+++ spec/models/slice_model_spec.rb
@@ -2,8 +2,9 @@ require "spec_helper"
 
 describe Slice do
     it "should reject slice with a body that exedes the story max sentences" do
-        story = Story.create!(:title => "Test Story", :max_sentences => 3)
-        slice = story.slices.create(:body => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.")
+        user = FactoryGirl.create(:user)
+        story = FactoryGirl.create(:story, :title => "Test Story", :max_sentences => 3)
+        slice = FactoryGirl.build(:slice, :body => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Integer posu    ere erat a ante venenatis dapibus posuere velit aliquet. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Cum sociis natoque penatibus et magni    s dis parturient montes, nascetur ridiculus mus.")
 
         slice.valid?
 
@@ -11,8 +12,9 @@ describe Slice do
     end
 
     it "should not reject slice with a body that is under or equal to the story max sentences" do
-        story = Story.create!(:title => "test story", :max_sentences => 10)
-        slice = story.slices.create(:body => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.")
+        user = FactoryGirl.create(:user)
+        story = FactoryGirl.create(:story, :title => "test story", :max_sentences => 10)
+        slice = FactoryGirl.build(:slice, :body => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Integer posu ere erat a ante venenatis dapibus posuere velit aliquet. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.")
 
         validation = slice.valid?
 
