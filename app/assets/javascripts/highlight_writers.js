6f033bda359e2178ab5af19645215dd981494ef9
jasonblanchard <jas.blanchard@gmail.com>
2013-03-21 15:34:41 -0400

    Doing something on rollover style

--- app/assets/javascripts/highlight_writers.js
+++ app/assets/javascripts/highlight_writers.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
+    var userId = '';
 
     if ($('.complete-story').length > 0) {
 
         $('div.slice').each(function() {
             $(this).hover(function() {
+                userId = $(this).attr('id');
+
                 $(this).addClass('muted');
+                $(".contributors #" + userId).addClass('active-contributor');
             }, function() {
                 $(this).removeClass('muted');
+                $(".contributors #" + userId).removeClass('active-contributor');
             });
 
         });
