a16480e7f4dae89b535027dfa6d1fbbf6a1a9d03
jasonblanchard <jas.blanchard@gmail.com>
2013-03-21 13:28:56 -0400

    DRYd up listing stories

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
 
     def home
-        @complete_stories = Story.where("complete = ?", true).limit(5).order('created_at DESC')
-        @incomplete_stories = Story.where("complete = ?", false).limit(5).order('created_at DESC')
+        @incomplete_stories = Story.story_list(false,5)
+        @complete_stories = Story.story_list(true,5)
         @excerpt = Story.all(:conditions => {:complete => true}).last
 
         if user_signed_in?
