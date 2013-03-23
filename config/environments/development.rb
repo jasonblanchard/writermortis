44ba181e8ae7320cb0005fb067ab078bd07b01c1
jasonblanchard <jas.blanchard@gmail.com>
2013-03-23 18:28:20 -0400

    Added config for gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,15 @@ Exquisitecorpse::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => 'gmail.com',
+      :user_name            => ENV['GMAIL_USERNAME'],
+      :password             => ENV['GMAIL_PASSWORD'],
+      :authentication       => 'plain',
+      :enable_starttls_auto => true  }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
