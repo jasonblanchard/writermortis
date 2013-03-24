edbd7d2b3218e73c10091c61b589724118bdac3a
jasonblanchard <jas.blanchard@gmail.com>
2013-03-24 14:39:14 -0400

    Fixed seed typos

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,7 @@ finished_story.save!
 
 bodies = [
     { :user_id => anon.id, :body => "Once upon a time, there was a catfish." },
-    { :user_id => user.id, :body => "And he loved building and casetles.\n\nOne day, he built a huge sandcasetle, big enough to live in it." },
+    { :user_id => user.id, :body => "And he loved building sand castles.\n\nOne day, he built a huge sandcastle, big enough to live in it." },
     { :user_id => anon.id, :body => "And so that's what he did.\n\nThe end."}
 ]
 
@@ -34,7 +34,7 @@ unless finished_story.done?
     end
 end
 
-incomplete_story = Story.find_or_create_by_title("The 13 parts.")
+incomplete_story = Story.find_or_create_by_title("The 13 parts")
 incomplete_story.user_id = user.id
 incomplete_story.total_slices = 13
 incomplete_story.save!
