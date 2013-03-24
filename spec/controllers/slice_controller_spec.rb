57edd88dc7a04d34319f820a1d1a72ec0e6461db
jasonblanchard <jas.blanchard@gmail.com>
2013-03-24 10:46:03 -0400

    DRYing up emails

---
+++ spec/controllers/slice_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe SlicesController do
+
+    describe "Sending emails" do
+        it "should send emails when a slice is added"
+        it "should send emails when a story is finished"
+    end
+end
---
+++ test/functional/story_mailer_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class StoryMailerTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
