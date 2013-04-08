49ce40678733365019045cea2d46f924716c8f5f
jasonblanchard <jas.blanchard@gmail.com>
2013-04-08 15:34:16 -0400

    Added before filter for staging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+    protect_from_forgery
+
+    before_filter :http_authenticate
+
+    def http_authenticate
+        if Rails.env.staging?
+            authenticate_or_request_with_http_basic do |username, password|
+                username == ENV['HTTP_USERNAME'] && password == ENV['HTTP_PASSWORD']
+            end
+        end
+    end
 end
